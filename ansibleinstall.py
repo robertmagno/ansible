5c98b8f7897a751941254a7849ee91bff482a469
Rob Magno <41025018+robertmagno@users.noreply.github.com>
2020-12-09 21:45:38 -0500

    Update ansibleinstall.py

--- ansibleinstall.py
+++ ansibleinstall.py
@@ -9,30 +9,30 @@ class SSH:
         
 def main():
   print("***Installing software dependencies***")
-	os.system("sudo apt-get install software-properties-common -y > /dev/null")
-	print("Done")
+  os.system("sudo apt-get install software-properties-common -y > /dev/null")
+  print("Done")
 	
-	print("***Adding Ansible Repository***")
-	os.system("sudo apt-add-repository ppa:ansible/ansible > /dev/null")
-	print("Done")	
+  print("***Adding Ansible Repository***")
+  os.system("sudo apt-add-repository ppa:ansible/ansible > /dev/null")
+  print("Done")	
   
-	os.system("sudo apt-get update -y > /dev/null")  
+  os.system("sudo apt-get update -y > /dev/null")  
   
-	print("***Installing Ansible***")
-	os.system("$ sudo apt install ansible -y > /dev/null")
-	print("Done")
+  print("***Installing Ansible***")
+  os.system("$ sudo apt install ansible -y > /dev/null")
+  print("Done")
 	
-	print("***Installing pip***")
-	os.system("sudo apt install python-pip -y > /dev/null")
-	print("Done")
+  print("***Installing pip***")
+  os.system("sudo apt install python-pip -y > /dev/null")
+  print("Done")
 	
   print("***Cloning Citrix ADC Ansible Repository***")
-	os.system("git clone https://github.com/citrix/citrix-adc-ansible-modules.git > /dev/null")
-	print("Done")
+  os.system("git clone https://github.com/citrix/citrix-adc-ansible-modules.git > /dev/null")
+  print("Done")
   
-	print("***Install Nitro API Python Dependencies***")
-	os.system("pip install /home/ubuntu/ansible/citrix-adc-ansible-modules/deps/nitro-python-1.0_kamet.tar.gz")
-	print("Done")
+  print("***Install Nitro API Python Dependencies***")
+  os.system("pip install /home/ubuntu/ansible/citrix-adc-ansible-modules/deps/nitro-python-1.0_kamet.tar.gz")
+  print("Done")
 
 
 #
